b8c7ab76d93cbabe4d8e84f8babdc1ba21a26883
Drew Chandler <andrewschandler@gmail.com>
2011-01-11 11:32:33 -0800

    Add some color to the prompt

--- bash/variables.bash
+++ bash/variables.bash
@@ -2,4 +2,4 @@ export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM=auto
-export PS1='\u@\h:\W$(__git_ps1 "(%s)")\$ '
+export PS1="$C5\$(date +%H:%M) $C10[\u@\h \W]$C8 \$(__git_ps1 '(%s)') $P\$ "
