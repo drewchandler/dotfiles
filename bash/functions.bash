7fc28f69a36313b4b3afc007b67d6315fe824463
Drew Chandler <drew@kongregate.com>
2011-10-26 11:26:14 -0700

    Add secure vault function

---
+++ bash/functions.bash
@@ -0,0 +1,8 @@
+function sv () {
+    SECURE_MOUNT_INFO=`hdid $VAULT_DMG | tail -n 1`;
+    SECURE_MOUNT_DEVICE=`echo -e $SECURE_MOUNT_INFO | cut -d ' ' -f1`;
+    SECURE_MOUNT_PATH=`echo -e $SECURE_MOUNT_INFO | cut -d ' ' -f3`;
+    bash --init-file $SECURE_MOUNT_PATH/.profile;
+    hdiutil eject $SECURE_MOUNT_DEVICE &> /dev/null;
+    unset SECURE_MOUNT_INFO SECURE_MOUNT_PATH SECURE_MOUNT_DEVICE
+}
---
+++ profile
@@ -0,0 +1,7 @@
+[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
+
+source ~/.bash/git-completion.bash
+source `brew --prefix`/Library/Contributions/brew_bash_completion.sh
+source ~/.bash/variables.bash
+source ~/.bash/functions.bash
+source ~/.bash/aliases.bash
