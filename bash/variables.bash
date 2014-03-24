5baa602f76a2cdcd2a190a6027430acf1cdcf467
Drew Chandler <drew@kongregate.com>
2014-03-24 10:34:53 -0700

    Add cabal to PATH and rearrange variables file

--- bash/variables.bash
+++ bash/variables.bash
@@ -1,13 +1,21 @@
+export PATH=/usr/local/bin:$PATH
+export PATH=/Applications/Postgres93.app/Contents/MacOS/bin:$PATH
+export PATH=$HOME/.rbenv/bin:$PATH
+export PATH=$HOME/.cabal/bin:$PATH
+
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM=auto
+
 YELLOW='\[\033[33m\]'
 CYAN='\[\033[36m\]'
 GREEN='\[\033[32m\]'
 WHITE='\[\033[37m\]'
 export PS1="$YELLOW\$(date +%H:%M) $CYAN[\u@\h \W]$GREEN \$(__git_ps1 '(%s)')\n$WHITE\$ "
-export PATH=/Applications/Postgres93.app/Contents/MacOS/bin:/usr/local/bin:$HOME/.rbenv/bin:$PATH
+
 export NODE_PATH=/usr/local/lib/node
+
 export EDITOR='mvim -f'
+
 VAULT_DMG=~/vault.dmg
