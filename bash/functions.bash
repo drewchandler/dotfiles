6c92714f2ebc95cac4261ffaafe0fefdf75670af
Drew Chandler <drew@kongregate.com>
2015-04-09 09:44:27 -0700

    Sentinel function

--- bash/functions.bash
+++ bash/functions.bash
@@ -14,3 +14,31 @@ function v {
     mvim --servername $(basename $(pwd)) --remote-silent "$@" 1>/dev/null 2>&1
   fi
 }
+
+function stl {
+  local SENTINEL_DIRECTORY="/Users/drew/work/kongregate/config/sentinels"
+  if [ -n "$1" ]; then
+    local SENTINEL="$SENTINEL_DIRECTORY/$1"
+    if [[ ! -f $SENTINEL ]]; then
+      local STATUS="\033[1;32mON\033[0m"
+      touch $SENTINEL
+    else
+      local STATUS="\033[1;31mOFF\033[0m"
+      rm $SENTINEL
+    fi
+    echo -e "$1 is now $STATUS"
+  fi
+  echo -en "\033[1;37mWorking sentinels:\033[0m "
+  echo "show_ads cache_classes error_pages screw_routes disable_ssl"
+  echo -en "\033[1;37mEnabled sentinels:\033[0m "
+  echo -n `ls $SENTINEL_DIRECTORY`
+  echo
+}
+
+function _stl
+{
+  COMPREPLY=(`compgen -W 'show_ads cache_classes error_pages screw_routes disable_ssl' -- ${COMP_WORDS[COMP_CWORD]}`)
+  return 0
+}
+
+complete -F _stl stl
