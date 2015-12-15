d68979d76217d17d29b4a82d171457ac3974ae13
Drew Chandler <andrewschandler@gmail.com>
2015-12-15 11:03:16 -0800

    Add some aliases for switching between branches

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -9,6 +9,8 @@ alias gdc='git diff --cached'
 alias gp='git push'
 alias gpu='git push -u origin `git rev-parse --abbrev-ref HEAD`'
 alias grc='git rebase --continue'
+alias grb="git for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short)'"
+alias srb='grb | selecta | xargs git co'
 alias retag='ctags -R --exclude .git --exclude tmp/ --exclude log/ .'
 alias nom='rm -rf node_modules && npm cache clear && npm install'
 alias bom='rm -rf bower_components && bower cache clean && bower install'
